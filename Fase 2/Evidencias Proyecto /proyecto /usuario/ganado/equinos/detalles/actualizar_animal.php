d817a79f9f7fe65a36feb9e14db73f93f3231a1f
Guillermo4251 <101352478+Guillermo4251@users.noreply.github.com>
2024-10-21 16:43:37 +0000

    Cambio de evidencias

---
+++ Fase 2/Evidencias Proyecto /proyecto /usuario/ganado/equinos/detalles/actualizar_animal.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("Location: https://nukatech.cl/citt/");
+    exit;
+}
+
+$conexion = new mysqli("localhost", "cbu91029_cberrios", "carlitrox21", "cbu91029_citt");
+
+if ($conexion->connect_error) {
+    die("La conexi¨®n ha fallado: " . $conexion->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ID_animal'])) {
+    $id_animal = $_POST['ID_animal'];
+    
+    $nombre = $_POST['nombre'];
+    $fecha = $_POST['fecha'];
+    $subtipo = $_POST['subtipo'];
+    $sexo = $_POST['sexo'];
+    
+
+    $sql = "UPDATE animales_trabajo 
+            SET  nombre = ?, fecha = ?, subtipo = ?, sexo = ? 
+            WHERE ID_animal = ?";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bind_param("sssss",  $nombre,  $fecha, $subtipo, $sexo, $id_animal);
+
+    if ($stmt->execute()) {
+        echo '<form id="redirectForm" action="index.php" method="post">';
+        echo '<input type="hidden" name="id_animal" value="' . htmlspecialchars($id_animal) . '">';
+        echo '<script type="text/javascript">';
+        echo 'document.getElementById("redirectForm").submit();';
+        echo '</script>';
+        echo '</form>';
+exit();
+        
+        
+    } else {
+        echo "<script>alert('Error al actualizar los datos.'); window.location.href = 'https://nukatech.cl/citt/usuario/ganado/equinos/';</script>";
+    }
+
+    $stmt->close();
+} else {
+    echo "<script>alert('No se recibi¨® ID de animal o la solicitud no es POST.'); window.location.href = 'https://nukatech.cl/citt/usuario/ganado/equinos/';</script>";
+}
+
+$conexion->close();
+?>
---
+++ Fase 2/Evidencias Proyecto /proyecto /usuario/mascotas/detalles/ficha_clinica/eliminar.php
@@ -0,0 +1,31 @@
+<?php
+$conexion = new mysqli("localhost", "cbu91029_cberrios", "carlitrox21", "cbu91029_citt");
+
+// Comprobar conexión
+if (!$conexion) {
+  die("La conexión ha fallado: " . mysqli_connect_error());
+}
+
+// Recibir datos del formulario
+$id = $_POST['id_ficha'];
+$id_animal = $_POST['id_animal'];
+// Preparar consulta SQL para inserción
+$sql = "DELETE FROM ficha_clinica_animal_trabajo WHERE id_ficha = '$id'"; 
+
+// Ejecutar consulta y comprobar si se ha insertado correctamente
+if (mysqli_query($conexion, $sql)) {
+    echo '<form id="redirectForm" action="index.php" method="post">';
+    echo '<input type="hidden" name="id_animal" value="' . htmlspecialchars($id_animal) . '">';
+    echo '<script type="text/javascript">';
+    echo 'document.getElementById("redirectForm").submit();';
+    echo '</script>';
+    echo '</form>';
+    exit();
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+}
+
+// Cerrar conexión
+mysqli_close($conexion);
+?>
+
