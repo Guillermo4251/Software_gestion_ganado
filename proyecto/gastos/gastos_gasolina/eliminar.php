68469e26b99471a8f3cb71e08e8a67a4ffcbe4a9
Guillermo4251 <101352478+Guillermo4251@users.noreply.github.com>
2024-11-25 22:32:01 -0300

    Back y front de control de gastos

---
+++ proyecto/gastos/gastos_gasolina/eliminar.php
@@ -0,0 +1,30 @@
+<?php
+$conexion = new mysqli("localhost", "cbu91029_cberrios", "carlitrox21", "cbu91029_citt");
+
+// Comprobar conexi車n
+if ($conexion->connect_error) {
+  die("La conexi車n ha fallado: " . $conexion->connect_error);
+}
+$id_gasto = $_POST['id_gasto'];
+
+// Recibir datos del formulario
+$sql = "DELETE FROM gastos_gasolina WHERE id_gasto = '$id_gasto'"; 
+
+
+// Ejecutar consulta y comprobar si se ha insertado correctamente
+if (mysqli_query($conexion, $sql)) {
+    $resultado = 'exito';
+} else {
+    echo 'Error: ' . $sql . "<br>" . mysqli_error($conexion);
+
+}
+
+
+// Cerrar conexi贸n
+mysqli_close($conexion);
+
+// Redirigir a la p谩gina animal.php
+header("Location: index.php?resultado=$resultado");
+exit;
+?>
+
